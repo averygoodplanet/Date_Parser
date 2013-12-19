574745dac28b4461dfa0a577ab3ded52c0c9381c
averygoodplanet <averygoodplanet@gmail.com>
2013-12-19 02:23:03 -0600

    test 1..7 passed

--- test_date_parser.rb
+++ test_date_parser.rb
@@ -2,20 +2,15 @@ require_relative 'date_parser'
 require 'minitest/autorun'
 
 class TestDateParser < MiniTest::Unit::TestCase
-  def test_1_date_parser_has_a_parse_method
-    DateParser.parse("12/22/2012")
-  end
 
 =begin
-  start with parsing a normal date format of mmDDYY
-  into local Fixnum variables month, day, and year
-  assuming month and day are both 2 digits and year is 4 digits
+  start with parsing a normal date format of mm/DD/YY
+  into local dateHash as Fixnum under keys month, day, and year
+  for 1-2 month or day, and 2 xor 4 digit year
 =end
 
-  def test_1b_check_passing_back_hash
-    assert_equal 12, (DateParser.parse("12/22/2012"))["month"]
-    assert_equal 22, (DateParser.parse("12/22/2012"))["day"]
-    assert_equal 2012, (DateParser.parse("12/22/2012"))["year"]
+  def test_1_date_parser_has_a_parse_method
+    DateParser.parse("12/22/2012")
   end
 
   def test_2_MMddYYYY_returns_2_digit_month
@@ -26,34 +21,27 @@ class TestDateParser < MiniTest::Unit::TestCase
     assert_equal 22, (DateParser.parse("12/22/2012"))["day"]
   end
 
-#   def test_4_MMddYYYY_returns_4_digit_year
-#     DateParser.parse("12/22/2012")
-#     assert_equal 2012, dateHash["year"]
-#     puts "Test 4"
-#   end
+  def test_4_MMddYYYY_returns_4_digit_year
+    assert_equal 2012,  (DateParser.parse("12/22/2012"))["year"]
+  end
 
 # =begin
 #   Next write tests for normal format month/day/year
 #   that can handle 1 digit month, 1 digit day, and 2 digit year
 # =end
 
-#   def test_5_MddYYYY_returns_1_digit_month
-#     DateParser.parse("1/22/2012")
-#     assert_equal 1, dateHash["month"]
-#     puts "Test 5"
-#   end
+  def test_5_MddYYYY_returns_1_digit_month
+    assert_equal 1, (DateParser.parse("1/22/2012"))["month"]
+  end
 
-#   def test_6_monthDyyyy_returns_1_digit_day
-#     DateParser.parse("1/2/2012")
-#     assert_equal 2, dateHash["day"]
-#     puts "Test 6"
-#   end
+  def test_6_monthDyyyy_returns_1_digit_day
+    assert_equal 2, (DateParser.parse("1/2/2012"))["day"]
+  end
 
-#   def test_7_monthDayYY_returns_2_digit_year
-#     DateParser.parse("1/2/95")
-#     assert_equal 95, dateHash["year"]
-#     puts "Test 7"
-#   end
+  def test_7_monthDayYY_returns_2_digit_year
+    DateParser.parse("1/2/95")
+    assert_equal 95, (DateParser.parse("1/2/95"))["year"]
+  end
 
 =begin
   look up standard format for month/day/year with time
